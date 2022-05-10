459998e2dae816912971068077cb4a1f7e8d14fe
Aztec1234 <ayushguidel@gmail.com>
2022-05-10 22:34:11 +0545

    Config server setup

---
+++ src/main/java/com/aztec/configserver/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.aztec.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/aztec/configserver/auth/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.aztec.configserver.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    //Securing the urls and allowing role-based access to these urls.
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(final HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable().authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    //In memory authentication to authenticate the user i.e. the user credentials are stored in the memory
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}user@123").roles("USER") //noOpPasswordEncoder -> no pwd encoder
+                .and()
+                .withUser("aztec").password(passwordEncoder().encode("aztec123")).roles("ADMIN");
+    }
+}
